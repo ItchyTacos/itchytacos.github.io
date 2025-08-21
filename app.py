23e9cfcd44f87684ebae2c9f383d1b88f40c1fe2
ItchyTacos <derpyarrow1@gmail.com>
2025-08-21 13:52:21 -0400

    added render_template method to add args

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def get():
-    with open("index.html") as file:
-        return file
+def index():
+    argname = request.args.get("name", "Flask")
+    return render_template("index.html", name = argname)
+    
+
+@app.route("/login")
+def login():
+    return render_template("error.html")
