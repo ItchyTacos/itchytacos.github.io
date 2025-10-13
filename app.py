412e65c59aee24b4e89aa0e433352202a5f8b572
ItchyTacos <derpyarrow1@gmail.com>
2025-10-13 14:26:30 -0400

    added style, update gitignore

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort
 from markupsafe import escape
 from werkzeug.security import check_password_hash
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+load_dotenv(".env")
+ADMIN_KEY = str(os.environ.get("ADMIN_KEY"))
+
+
+def check_ip(ip):
+    return ip.startswith("10.8.")
 
 @app.route("/")
 def index():
@@ -11,7 +20,12 @@ def index():
 
 @app.route("/login")
 def login():
-    ADMIN_KEY = "fortnite"
+    client_ip = request.remote_addr
+    print(client_ip)
+    #Check to see if this works when moving it to server
+    #YOU HAVE TO MAKE SURE WG WORKS FIRST
+    # if check_ip:
+    #     abort(403)
     username = request.args.get("username", "Flask")
     password = request.args.get("password", "")
     if check_password_hash(ADMIN_KEY, password):
@@ -19,3 +33,13 @@ def login():
         return render_template("error.html",username = escape(username))
     else:
         return render_template("error.html")
+    
+@app.errorhandler(404)
+def errorroute(e):
+    username = request.args.get("username", "Unknown User")
+    return render_template("error.html",username=username)
+
+@app.route("/test")
+def test():
+    password = str(request.args.get("password"))
+    return str(check_password_hash(ADMIN_KEY,password))
